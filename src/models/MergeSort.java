d9ab401cd06fcbeec4e76f4a4f837cb27ef3807c
cifelse <meetcif@gmail.com>
2024-01-30 23:31:30 +0800

    Create MergeSort.java

---
+++ src/models/MergeSort.java
@@ -0,0 +1,34 @@
+package models;
+
+public class MergeSort {
+    /**
+     * This function performs the merge operation of merge sort.
+     * @param array {Vector<Integer>} - array to sort
+     * @param s {Integer} - start index of merge
+     * @param e {Integer} - end index (inclusive) of merge
+     */
+    public static void merge(int[] array, int s, int e) {
+        int m = s + (e - s) / 2;
+        int[] left = new int[m - s + 1];
+        int[] right = new int[e - m];
+        int l_ptr = 0, r_ptr = 0;
+        for(int i = s; i <= e; i++) {
+            if(i <= m) {
+                left[l_ptr++] = array[i];
+            } else {
+                right[r_ptr++] = array[i];
+            }
+        }
+        l_ptr = r_ptr = 0;
+
+        for(int i = s; i <= e; i++) {
+            if(r_ptr == e - m || left[l_ptr] <= right[r_ptr]) {
+                array[i] = left[l_ptr];
+                l_ptr++;
+            } else {
+                array[i] = right[r_ptr];
+                r_ptr++;
+            }
+        }
+    }
+}
