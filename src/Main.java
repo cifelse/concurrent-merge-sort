59c944d742c298e71910ec2eb8619d80cdc3872c
cifelse <meetcif@gmail.com>
2024-01-30 23:31:51 +0800

    Add getUserInput function

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,39 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+    // Seed
+    private static final long SEED = 18;
+
+    /**
+     * The main function for getting an input from the user
+     * @param scanner - the Scanner Object
+     * @param prompt - question to prompt to the user
+     * @return the input of the user
+     */
+    public static int getUserInput(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        return scanner.nextInt();
+    }
+
     public static void main(String[] args) {
-        // TODO: Seed your randomizer
+        // Seed your randomizer
+        Random random = new Random(SEED);
 
-        // TODO: Get array size and thread count from user
+        // Get array size and thread count from user
+        Scanner sc = new Scanner(System.in);
 
-        // TODO: Generate a random array of given size
+        int nSize =  getUserInput(sc, "Enter size of array: ");
+
+        int nThreads = getUserInput(sc, "Enter number of threads: ");
+
+        // Close the Scanner
+        sc.close();
+
+        // Generate a random array of given size
+        int[] randomArray = new int[nSize];
 
         // TODO: Call the generate_intervals method to generate the merge 
         // sequence
@@ -16,107 +42,5 @@ public class Main {
 
         // Once you get the single-threaded version to work, it's time to 
         // implement the concurrent version. Good luck :)
-
-    }
-
-    /*
-    This function generates all the intervals for merge sort iteratively, given 
-    the range of indices to sort. Algorithm runs in O(n).
-
-    Parameters:
-    start : int - start of range
-    end : int - end of range (inclusive)
-
-    Returns a list of Interval objects indicating the ranges for merge sort.
-    */
-    public static List<Interval> generate_intervals(int start, int end) {
-        List<Interval> frontier = new ArrayList<>();
-        frontier.add(new Interval(start,end));
-
-        int i = 0;
-        while(i < frontier.size()){
-            int s = frontier.get(i).getStart();
-            int e = frontier.get(i).getEnd();
-
-            i++;
-
-            // if base case
-            if(s == e){
-                continue;
-            }
-
-            // compute midpoint
-            int m = s + (e - s) / 2;
-
-            // add prerequisite intervals
-            frontier.add(new Interval(m + 1,e));
-            frontier.add(new Interval(s,m));
-        }
-
-        List<Interval> retval = new ArrayList<>();
-        for(i = frontier.size() - 1; i >= 0; i--) {
-            retval.add(frontier.get(i));
-        }
-
-        return retval;
-    }
-
-    /*
-    This function performs the merge operation of merge sort.
-
-    Parameters:
-    array : vector<int> - array to sort
-    s     : int         - start index of merge
-    e     : int         - end index (inclusive) of merge
-    */
-    public static void merge(int[] array, int s, int e) {
-        int m = s + (e - s) / 2;
-        int[] left = new int[m - s + 1];
-        int[] right = new int[e - m];
-        int l_ptr = 0, r_ptr = 0;
-        for(int i = s; i <= e; i++) {
-            if(i <= m) {
-                left[l_ptr++] = array[i];
-            } else {
-                right[r_ptr++] = array[i];
-            }
-        }
-        l_ptr = r_ptr = 0;
-
-        for(int i = s; i <= e; i++) {
-            if(r_ptr == e - m || left[l_ptr] <= right[r_ptr]) {
-                array[i] = left[l_ptr];
-                l_ptr++;
-            } else {
-                array[i] = right[r_ptr];
-                r_ptr++;
-            }
-        }
-    }
-}
-
-class Interval {
-    private int start;
-    private int end;
-
-    public Interval(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-
-    public int getStart() {
-        return start;
-    }
-
-    public void setStart(int start) {
-        this.start = start;
-    }
-
-    public int getEnd() {
-        return end;
-    }
-
-    public void setEnd(int end) {
-        this.end = end;
     }
 }
