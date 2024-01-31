9c24df66f52fa638c912226e48e5317ea8af8a00
cifelse <meetcif@gmail.com>
2024-01-31 11:35:32 +0800

    Finish single-threaded MergeSort

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
-import models.Interval;
 import models.MergeSort;
 
 public class Main {
-    // Seed
+    // Seed for the Randomizer
     private static final long SEED = 18;
 
     /**
      * The main function for getting an input from the user
-     * @param scanner - the Scanner Object
-     * @param prompt - question to prompt to the user
+     * @param scanner {Scanner} - the Scanner Object
+     * @param prompt {String} - question to prompt to the user
      * @return the input of the user
      */
     public static int getUserInput(Scanner scanner, String prompt) {
@@ -22,47 +21,24 @@ public class Main {
     }
 
     /**
-     * This function generates all the intervals for merge sort iteratively, given the range of indices to sort. Algorithm runs in O(n).
-     * @param start {Integer} - Start of range
-     * @param end {Integer} - End of range
-     * @return - Returns a list of Interval objects indicating the ranges for merge sort.
+     * Shuffles an Array randomly
+     * @param array {Array<int>} - the array of integers to be shuffled
+     * @param seed {long} - Randomizer seed
      */
-    public static List<Interval> generateIntervals(int start, int end) {
-        List<Interval> frontier = new ArrayList<>();
-        frontier.add(new Interval(start, end));
+    public static void shuffle(int[] array, long seed) {
+        Random random = new Random(SEED);
 
-        int i = 0;
-        while(i < frontier.size()){
-            int s = frontier.get(i).getStart();
-            int e = frontier.get(i).getEnd();
+        for (int i = array.length - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
 
-            i++;
-
-            // if base case
-            if(s == e){
-                continue;
-            }
-
-            // compute midpoint
-            int m = s + (e - s) / 2;
-
-            // add prerequisite intervals
-            frontier.add(new Interval(m + 1, e));
-            frontier.add(new Interval(s, m));
+            // Swap Elements
+            int temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
         }
-
-        List<Interval> retval = new ArrayList<>();
-        for(i = frontier.size() - 1; i >= 0; i--) {
-            retval.add(frontier.get(i));
-        }
-
-        return retval;
     }
 
     public static void main(String[] args) {
-        // Seed your randomizer
-        Random random = new Random(SEED);
-
         // Get array size and thread count from user
         Scanner sc = new Scanner(System.in);
 
@@ -73,23 +49,30 @@ public class Main {
         // Close the Scanner
         sc.close();
 
-        // TODO: Generate a random array of given size
-        int[] randomArray = new int[nSize];
+        // Generate a random array of given size
+        int[] randomArray = java.util.stream.IntStream.range(1, nSize + 1).toArray();
+
+        // Shuffle Array
+        shuffle(randomArray, SEED);
+
+        // Display Before Array
+        System.out.println("\nBEFORE: " + Arrays.toString(randomArray));
+
+        // Start Timer
+        long startTime = System.nanoTime();
         
-        for (int i = 0; i < nSize; i++) {
-            randomArray[i] = random.nextInt(nSize);
-        }
+        // Call MergeSort
+        MergeSort.merge(randomArray, 0, randomArray.length - 1, nThreads);
+        
+        // End Timer
+        long endTime = System.nanoTime();
 
-        // TODO: Call the generate_intervals method to generate the merge sequence
-        List<Interval> sequence = generateIntervals(0, nSize - 1);
+        // Display After Array
+        System.out.println("AFTER: " + Arrays.toString(randomArray));
 
-        // TODO: Call merge on each interval in sequence
-        for (Interval interval: sequence)
-            MergeSort.merge(randomArray, interval.getStart(), interval.getEnd());
+        // Display Time and Results
+        System.out.printf("\nTotal Runtime: %d milliseconds.\n", (long) TimeUnit.NANOSECONDS.toMillis(endTime - startTime));
 
-        //For Checking:
-        for(int x :randomArray)
-            System.out.println(x);
         // Once you get the single-threaded version to work, it's time to 
         // implement the concurrent version. Good luck :)
     }
--- src/models/MergeSort.java
+++ src/models/MergeSort.java
@@ -1,13 +1,69 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MergeSort {
     /**
-     * This function performs the merge operation of merge sort.
-     * @param array {Vector<Integer>} - array to sort
-     * @param s {Integer} - start index of merge
-     * @param e {Integer} - end index (inclusive) of merge
+     * The main function that executes MergeSort
+     * @param array {Array<int>} - The Array to be sorted
+     * @param start - {int} Start range: 0
+     * @param end - {int} End range: Array Length
+     * @param nThreads {int} - The number of Threads to be implemented
      */
-    public static void merge(int[] array, int s, int e) {
+    public static void merge(int[] array, int start, int end, int nThreads) {
+        List<Interval> intervals = generateIntervals(start, end);
+
+        for (Interval interval : intervals) {
+            sort(array, interval.getStart(), interval.getEnd());
+        }
+    }
+
+    /**
+     * This function generates all the intervals for merge sort iteratively, given the range of indices to sort. Algorithm runs in O(n).
+     * @param start {int} - Start of range
+     * @param end {int} - End of range
+     * @return - Returns a list of Interval objects indicating the ranges for merge sort.
+     */
+    private static List<Interval> generateIntervals(int start, int end) {
+        List<Interval> frontier = new ArrayList<>();
+        frontier.add(new Interval(start, end));
+
+        int i = 0;
+        while(i < frontier.size()){
+            int s = frontier.get(i).getStart();
+            int e = frontier.get(i).getEnd();
+
+            i++;
+
+            // if base case
+            if(s == e){
+                continue;
+            }
+
+            // compute midpoint
+            int m = s + (e - s) / 2;
+
+            // add prerequisite intervals
+            frontier.add(new Interval(m + 1, e));
+            frontier.add(new Interval(s, m));
+        }
+
+        List<Interval> retval = new ArrayList<>();
+        for(i = frontier.size() - 1; i >= 0; i--) {
+            retval.add(frontier.get(i));
+        }
+
+        return retval;
+    }
+
+    /**
+     * This function performs the merge operation of merge sort.
+     * @param array {Vector<int>} - array to sort
+     * @param s {int} - start index of merge
+     * @param e {int} - end index (inclusive) of merge
+     */
+    private static void sort(int[] array, int s, int e) {
         int m = s + (e - s) / 2;
         int[] left = new int[m - s + 1];
         int[] right = new int[e - m];
